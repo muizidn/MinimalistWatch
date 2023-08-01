2a2af990585b7cf36a7ced07ef4a7eec8511189a
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-01 21:35:20 +0700

    Initial Commit

---
+++ Minimalist Watch/Minimalist_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimalist_WatchApp.swift
+//  Minimalist Watch
+//
+//  Created by Muhammad Muizzsuddin on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Minimalist_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
